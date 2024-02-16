3a053656917144412d1cb7ba496882a06fcb8f7e
Jatan Dudeja <dudeja.jatan04@gmail.com>
2024-02-16 13:38:59 +0530

    added server search.

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,60 +1,125 @@
-import { currentProfile } from "@/lib/current-profile"
+import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHeader } from "./server-header";
+import { ScrollArea } from "../ui/scroll-area";
+import { ServerSearch } from "./server-search";
+import { Hash, Mic, Shield, ShieldAlert, Video } from "lucide-react";
 
 interface ServerSidebarProps {
-    serverId : string
+  serverId: string;
 }
 
-export const ServerSidebar = async ({ serverId } : ServerSidebarProps) => {
-    const profile = await currentProfile();
+const iconMap = {
+    [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4"/>,
+    [ChannelType.AUDIO]: <Mic className="mr-2 h-4 w-4"/>,
+    [ChannelType.VIDEO]: <Video className="mr-2 h-4 w-4"/>
+};
 
-    if(!profile){
-        return redirect("/")
-    }
+const roleIconMap = {
+    [MemberRole.GUEST] : null,
+    [MemberRole.MODERATOR] : <Shield className="h-4 w-4 mr-2 text-indigo-500"/>,
+    [MemberRole.ADMIN] : <ShieldAlert className="h-4 w-4 mr-2 text-rose-500"/>
+}
 
-    const server = await db.server.findUnique({
-        where: {
-            id: serverId
+export const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirect("/");
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
         },
+      },
+      members: {
         include: {
-            channels: {
-                orderBy: {
-                    createdAt: "asc"
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videoChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
+
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile.id
+  );
+
+  if (!server) {
+    return redirect("/");
+  }
+
+  const role = server.members.find(
+    (member) => member.profileId === profile.id
+  )?.role;
+
+  return (
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch data={
+            [
+                {
+                    label: "Text Channels",
+                    type: "channel",
+                    data: textChannels?.map((channel) => ({
+                        id: channel.id,
+                        name: channel.name,
+                        icon: iconMap[channel.type]
+                    }))
                 },
-            },
-            members: {
-                include: {
-                    profile: true,
+                {
+                    label: "Voice Channels",
+                    type: "channel",
+                    data: audioChannels?.map((channel) => ({
+                        id: channel.id,
+                        name: channel.name,
+                        icon: iconMap[channel.type]
+                    }))
                 },
-                orderBy: {
-                    role: "asc",
-                }
-            }
-        }
-    })
-
-
-const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
-const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
-const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO)
-
-const members = server?.members.filter((member) => member.profileId !== profile.id)
-
-if(!server){
-    return redirect("/")
-}
-
-const role = server.members.find((member) => member.profileId === profile.id)?.role;
-
-    return (
-        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
-            <ServerHeader
-                server={server}
-                role={role}
-            />
+                {
+                    label: "Video Channels",
+                    type: "channel",
+                    data: videoChannels?.map((channel) => ({
+                        id: channel.id,
+                        name: channel.name,
+                        icon: iconMap[channel.type]
+                    }))
+                },
+                {
+                    label: "Members",
+                    type: "member",
+                    data: members?.map((member) => ({
+                        id: member.id,
+                        name: member.profile.name,
+                        icon: roleIconMap[member.role]
+                    }))
+                },
+            ]
+          }/>
         </div>
-    )
-}
+      </ScrollArea>
+    </div>
+  );
+};
